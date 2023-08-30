db1cfb970b7aaa711bbea200a8b24ede10efbdbc
zaidkhan2625 <zaidkhan262523@gmail.com>
2023-08-30 18:00:49 +0530

    js code

---
+++ datatype.js
@@ -0,0 +1,39 @@
+//java data type int ,float ,char ,double , String ,boolean
+//number  ,string,boolean, undefined object(key value pair),
+// let k=10.5;
+// console.log(typeof k);
+// let kk = "zaid";
+// console.log(typeof kk);
+// let zz = 'c';
+// console.log(typeof zz);
+// let l ;
+// console.log(l);
+// let obj ={
+//     name:'zaid',
+//     email:"zaidkkha"
+// }
+// console.log(typeof obj)
+
+// let arr1 =[1,2,3,4];
+// console.log(typeof arr1);
+// let nan =NaN;
+// console.log(nan)
+
+// // variable type in js
+// let ,const,var 
+// var k= 5;
+//  k=10;
+// console.log(k)
+
+// let
+// let k = 10;
+//   k=9;
+//   console.log(k);
+
+// const
+
+let k= [10,19,10,17];
+ k[0] = 199;
+console.log(k);
+
+// assigenment let var char diffrence chart
